fd0def1a311dc5520278b47944d5e834914955e4
Reijo Keskitalo <reijo.keskitalo@gmail.com>
2018-01-12 12:54:42 -0800

    Add barrier

--- pipelines/toast_test_import.py
+++ pipelines/toast_test_import.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import sys
+
 print('Checkpoint # 0 : importing toast.mpi', flush=True)
 
 from toast.mpi import MPI
@@ -11,3 +13,12 @@ import toast
 
 print('{:5} : Checkpoint # 2 : toast imported'.format(MPI.COMM_WORLD.rank),
       flush=True)
+
+MPI.COMM_WORLD.Barrier()
+
+if MPI.COMM_WORLD.rank == 0:
+    print('ALL IMPORTS SUCCESFULL')
+
+MPI.COMM_WORLD.Barrier()
+
+sys.exit()
